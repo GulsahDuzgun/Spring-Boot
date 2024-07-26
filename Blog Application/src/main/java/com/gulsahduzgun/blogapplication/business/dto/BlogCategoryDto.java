c00b20a934a0d5f473f7e6e5f07d953467c146eb
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-07-26 11:11:06 +0300

    BlogCategoryDto,BlogCategoryEntity

---
+++ Blog Application/src/main/java/com/gulsahduzgun/blogapplication/business/dto/BlogCategoryDto.java
@@ -0,0 +1,31 @@
+package com.gulsahduzgun.blogapplication.business.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Log4j2
+@Builder
+
+public class BlogCategoryDto implements Serializable {
+
+    public static final Long serialVersionUID = 1L;
+    private Long categoryID;
+
+    @Builder.Default
+    private Date systemCreatedDate = new Date(System.currentTimeMillis());
+
+    @NotEmpty(message = "{blog.category.validation.constraints.NotNull.message}")
+    @Size(min = 2, message = "{blog.category.least.validation.constraints.NotNull.message}")
+    private  String categoryName;
+}
---
+++ Blog Application/src/main/java/com/gulsahduzgun/blogapplication/data/entity/BlogCategoryEntity.java
@@ -0,0 +1,39 @@
+package com.gulsahduzgun.blogapplication.data.entity;
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serializable;
+import java.util.Date;
+
+// LOMBOK
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Log4j2
+@Builder
+
+@Entity(name="BlogCategories")
+@Table(name="blogCategories")
+
+// BlogCategoryDto(1)- BlogDto(N)
+public class BlogCategoryEntity implements  Serializable{
+    public static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Column(name="category_id",unique = true,nullable = false, updatable = false, insertable = true)
+    private  Long categoryID;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private Date systemCreateDate;
+
+    @Column(name="category_name")
+    private  String categoryName;
+}
