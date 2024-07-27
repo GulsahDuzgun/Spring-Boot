4de8e7301f433ccd250d7fc8f48a36682fa8f918
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-07-27 15:44:05 +0300

    fix

--- To-Do/src/main/java/com/gulsahduzgun/todo/ToDoApplication.java
+++ To-Do/src/main/java/com/gulsahduzgun/todo/ToDoApplication.java
@@ -1,12 +1,28 @@
 package com.gulsahduzgun.todo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.util.TimeZone;
+
+@SpringBootApplication(exclude = {
+        //SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+
+
+//@SpringBootApplication
 public class ToDoApplication {
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+    }
 
     public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "false");
         SpringApplication.run(ToDoApplication.class, args);
     }
 
---
+++ To-Do/src/main/java/com/gulsahduzgun/todo/bean/SwaggerBean.java
@@ -0,0 +1,33 @@
+package com.gulsahduzgun.todo.bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerBean {
+
+    @Bean
+    public OpenAPI getOpenAPIMethod() {
+        return  new OpenAPI().info(new Info()
+                .title("Advanced Fullstack Mastery Bootcamp by Hamit Mızrak")
+                .version("v1.0.0")
+                .description("Spring Boot & React.js")
+                .summary(" for spring boot on react js, auth: "+hashCode())
+                .description("Spring Boot & React Js & Devops")
+                .termsOfService(" Software INC")
+                .contact(new Contact()
+                        .name("Gülşah Düzgün")
+                        .email("test@gmail.com")
+                        .url("https://github.com/GulsahDuzgun")
+                )
+                .license(new License()
+                        .name("licence INC xyz")
+                        .url("https://github.com/GulsahDuzgun")
+                )
+        );
+    }
+}
