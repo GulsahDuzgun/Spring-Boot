5c0a1156ec9334fba4e5f38b98938e67c3cdd610
GulsahDuzgun <duzgun.gulsah27@gmail.com>
2024-08-03 14:59:15 +0300

    statusTag is added

--- To-Do/To-Do App/src/components/TaskItem.jsx
+++ To-Do/To-Do App/src/components/TaskItem.jsx
@@ -1,13 +1,30 @@
 import { HiMiniTrash, HiMiniPencilSquare } from "react-icons/hi2";
 
+const statusColors = {
+  not_started: "#ff8787",
+  in_progress: "#ffd43b",
+  done: "#69db7c",
+};
+
 export default function TaskItem({ tasks }) {
   return tasks?.map((task, indx) => (
-    <li className='task' key={indx}>
-      <div className='task--detail'>
-        <p className='task--title'>{task.taskName}</p>
-        <p className='task--text'>{task.task}</p>
+    <li className="task" key={indx}>
+      <div className="task__detail">
+        <span
+          className="task__status"
+          style={{
+            backgroundColor:
+              statusColors[
+                `${task?.statusName?.replace(" ", "_").toLowerCase()}`
+              ],
+          }}
+        >
+          {task.statusName}
+        </span>
+        <p className="task__title">{task.taskName}</p>
+        <p className="task__text">{task.task}</p>
       </div>
-      <div className='task--btn'>
+      <div className="task__btn">
         <button>
           <HiMiniPencilSquare />
         </button>
